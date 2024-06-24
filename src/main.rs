5b1b2c305db0120d1691607c1559f32b92362fe0
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2024-06-25 08:00:02 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Crab!!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
